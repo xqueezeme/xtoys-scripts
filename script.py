23fbd47217ce3b7f649b9317adc40b316c8ad48e
xqueezeme <xqueezeme>
2022-11-07 03:35:46 +0100

    Automatic update

--- script.py
+++ script.py
@@ -20,8 +20,6 @@ import traceback
 import os
 import sys
 from pyvirtualdisplay import Display
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
 
 #display = Display(visible=0, size=(800, 600))
 #display.start()
@@ -38,8 +36,7 @@ options = webdriver.ChromeOptions()
 options.add_argument("--mute-audio")
 options.add_argument('--disable-browser-side-navigation')
 options.add_argument("--headless")
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().arm64().install()), options=options)
-
+driver = webdriver.Chrome(options=options)
 def getPage(url):
     try:
         response = session.get(url)
