6b0f74630b3bb0af347883eecc307e27c8d4baa5
xqueezeme <excusemi@github.com>
2023-05-29 00:32:23 +0200

    test

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ git push origin
 git pull origin
 #python3 -m pip install --upgrade pip
 #python3 -m pip install -r requirements.txt
-unbuffer python3 script.py |& tee -a script.log
+python3 script.py
 git add .
 git commit -am "Automatic update"
 git push origin
