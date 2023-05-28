d8fd9db14b3d1ca4c0c5b7e1eefc73c0ab3b571d
xqueezeme <xqueezeme>
2023-05-28 15:03:21 +0200

    scripts

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 killall chromimum-browser
 git pull origin
-python3 -m pip install --upgrade pip
-python3 -m pip install -r requirements.txt
+#python3 -m pip install --upgrade pip
+#python3 -m pip install -r requirements.txt
 unbuffer python3 script.py |& tee -a script.log
 git add .
 git commit -am "Automatic update"
