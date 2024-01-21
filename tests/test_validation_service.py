4cb4ca038c47519fac49705bdbde4e468a1f78a6
xqueezeme <excusemi@github.com>
2024-01-21 17:03:50 +0100

    test

--- tests/test_validation_service.py
+++ tests/test_validation_service.py
@@ -20,31 +20,57 @@ class Test(TestCase):
             "name": "Lily Kawaii - POV Asian Blowjob",
             "site": "eporner",
             "id": "YxulHyEGKVv",
-            "scripts": [
-                {
-                    "name": "Lily Kawaii - POV Asian Blowjob.funscript",
-                    "location": "https://raw.githubusercontent.com/xqueezeme/xtoys-scripts/main/funscripts/lily-kawaii-pov-asian-blowjob-1.funscript"
-                }
-            ],
-            "tags": [
-                "blowjob",
-                "pov",
-                "asian",
-                "action",
-                "facial"
-            ],
-            "created_at": "2024-01-20T05:54:59.378Z",
-            "url": "https://discuss.eroscripts.com/t/lily-kawaii-pov-asian-blowjob/142542",
-            "valid": False,
-            "creator": "TheFunscripter",
-            "ignore": False,
-            "last_checked": "2024-01-20T22:55:02.964229",
-            "thumbnail": "https://raw.githubusercontent.com/xqueezeme/xtoys-scripts/main/images/lily-kawaii-pov-asian-blowjob.jpeg"
+            "valid": False
         }
         validation_service.validateVideo(driver,
                                          video
                                          )
         self.assertTrue(video.get('valid'))
+
+    def test_validate_video_spankbang(self):
+        chromedriver_binary_sync.download()
+
+        driver = webdriver.Chrome()
+        video = {
+            "name": "CH Island 5 Episode I",
+            "id": "3s6gd",
+            "site": "spankbang",
+            "valid": False
+        }
+        validation_service.validateVideo(driver,
+                                         video
+                                         )
+        self.assertTrue(video.get('valid'))
+
+    def test_validate_video_pornhub(self):
+        chromedriver_binary_sync.download()
+
+        driver = webdriver.Chrome()
+        video = {
+            "name": "Eva Elfie - Tries a Big Cock inside her Tight Pussy",
+            "site": "pornhub",
+            "id": "ph5cdf07d5bbe31",
+            "valid": False
+        }
+        validation_service.validateVideo(driver,
+                                         video
+                                         )
+        self.assertTrue(video.get('valid'))
+
+    def test_validate_video_pornhub_invalid(self):
+        chromedriver_binary_sync.download()
+
+        driver = webdriver.Chrome()
+        video = {
+            "name": "invalid",
+            "site": "pornhub",
+            "id": "ph5cdf07d5bbe31xxxxx",
+            "valid": False
+        }
+        validation_service.validateVideo(driver,
+                                         video
+                                         )
+        self.assertFalse(video.get('valid'))
     def test_validate_video_xhamster(self):
         chromedriver_binary_sync.download()
 
@@ -54,24 +80,7 @@ class Test(TestCase):
             "name": "Jenna Jameson - POV blowjob",
             "site": "xhamster",
             "id": "xhG64R9",
-            "scripts": [
-                {
-                    "name": "Jenna Jameson - POV blowjob.funscript",
-                    "location": "https://raw.githubusercontent.com/xqueezeme/xtoys-scripts/main/funscripts/jenna-jameson-pov-blowjob-1.funscript"
-                }
-            ],
-            "tags": [
-                "blowjob",
-                "pov",
-                "handjob",
-                "shefinishesthejob"
-            ],
-            "created_at": "2021-04-16T23:00:43.980Z",
-            "url": "https://discuss.eroscripts.com/t/xxx/22052",
-            "valid": False,
-            "creator": "burtreynolds",
-            "ignore": False,
-            "last_checked": "2024-01-14T02:34:27.977620"
+            "valid": False
         }
         validation_service.validateVideo(driver,
                                          video
@@ -83,13 +92,13 @@ class Test(TestCase):
 
         driver = webdriver.Chrome()
         sourceIndexFile = '../index-source.json'
-        validation_service.validate_selenium(driver, sourceIndexFile, "eporner")
+        #validation_service.validate_selenium(driver, sourceIndexFile, "eporner")
 
     def test_validate_videos_spankbang(self):
         chromedriver_binary_sync.download()
 
         driver = webdriver.Chrome()
         sourceIndexFile = '../index-source.json'
-        validation_service.validate_selenium(driver, sourceIndexFile, "spankbang")
+        #validation_service.validate_selenium(driver, sourceIndexFile, "spankbang")
 
 
